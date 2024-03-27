ab4f1bd132b1c85417f7444e8bd6d08601c34108
Kostas Makedos <kostasmakedos@cloud.upwork.com>
2024-03-27 23:54:22 +0200

    Comment out all modules

--- ApplicationLayers/accounts/athtech/periklis/java-app.tf
+++ ApplicationLayers/accounts/athtech/periklis/java-app.tf
@@ -1,7 +1,8 @@
-module "java-app" {
-  source           = "../../../modules/app"
-  vpc_name         = var.vpc_name
-  application_name = "${var.env_name}-java"
-  ingress_ports    = ["80"]
-  common_tags      = var.common_tags
-}
+#module "java-app" {
+#  source           = "../../../modules/app"
+#  vpc_name         = var.vpc_name
+#  application_name = "${var.env_name}-java"
+#  ingress_ports    = ["80"]
+#  common_tags      = var.common_tags
+#}
+#
